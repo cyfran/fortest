1088086eff32d74237f4b55e324ccac09795a851
cyfran <eremiya@gmail.com>
2015-05-15 19:19:41 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 /bin/mkdir /usr/local/ispmgr
 /usr/bin/wget -O /usr/local/ispmgr/install.tgz http://download.ispsystem.com/Linux-cc6/x86_64/ISPmanager-Lite/install.tgz
 /bin/tar -C /usr/local/ispmgr/ -xzf /usr/local/ispmgr/install.tgz
+/bin/rm -f /usr/local/ispmgr/install.tgz
